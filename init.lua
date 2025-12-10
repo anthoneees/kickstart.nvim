095e4c7f70157822744048ec8e3f2de79361ff20
Anthony <anthon.ee.es@gmail.com>
2025-12-09 19:28:40 -0500

    added hyprlang treesitter, and lsp

--- init.lua
+++ init.lua
@@ -690,6 +690,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         gopls = {},
+        hyprls = {},
         eslint = {},
         ts_ls = {},
         html = {},
@@ -948,7 +949,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'hyprlang' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
